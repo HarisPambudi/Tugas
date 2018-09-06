ba582a547169f4362adf6616f85793dd1d77e95f
HarisPambudi <43019693+HarisPambudi@users.noreply.github.com>
2018-09-06 09:53:55 +0700

    Add files via upload

---
+++ Haris.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author asus
+ */
+public class Haris {
+    
+
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int y=5;
+        for (int x=1;x<=5;x++){
+            for(int z=1;x<=y;z++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
